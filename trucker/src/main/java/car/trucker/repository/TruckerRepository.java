3a62394a2411f3c7075d300875e568e2dcb0121d
AvinashTgk <gt8y2@mail.umkc.edu>
2018-06-03 21:26:19 -0500

    commit

--- trucker/src/main/java/car/trucker/repository/TruckerRepository.java
+++
@@ -1,7 +0,0 @@
-package car.trucker.repository;
-
-import car.trucker.entity.TruckerVehicles;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TruckerRepository extends CrudRepository<TruckerVehicles, String> {
-}
